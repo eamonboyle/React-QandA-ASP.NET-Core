6d50772a50a00b8d6837a0810a276faca8e2130c
Eamon Boyle <blaowskate@hotmail.com>
2021-05-07 21:47:44 +0100

    Add lazy loading to the ask page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,19 @@
 import { css } from "@emotion/react";
 import { fontFamily, fontSize, gray2 } from "./Data/Styles";
 
+import React from "react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { HomePage } from "./Components/Pages/HomePage";
-import { AskPage } from "./Components/Pages/AskPage";
+// import { AskPage } from "./Components/Pages/AskPage";
 import { SearchPage } from "./Components/Pages/SearchPage";
 import { SignInPage } from "./Components/Pages/SignInPage";
 import { NotFoundPage } from "./Components/Pages/NotFoundPage";
 import { QuestionPage } from "./Components/Pages/QuestionPage";
 import { Header } from "./Components/Layout/Header";
+
+const AskPage = React.lazy(() => import("./Components/Pages/AskPage"));
 
 const App: React.FC = () => {
   return (
@@ -26,7 +30,25 @@ const App: React.FC = () => {
         <Routes>
           <Route path="" element={<HomePage />} />
           <Route path="search" element={<SearchPage />} />
-          <Route path="ask" element={<AskPage />} />
+          <Route
+            path="ask"
+            element={
+              <React.Suspense
+                fallback={
+                  <div
+                    css={css`
+                      margin-top: 100px;
+                      text-align: center;
+                    `}
+                  >
+                    Loading...
+                  </div>
+                }
+              >
+                <AskPage />
+              </React.Suspense>
+            }
+          />
           <Route path="signin" element={<SignInPage />} />
           <Route path="questions/:questionId" element={<QuestionPage />} />
           <Route path="*" element={<NotFoundPage />} />
